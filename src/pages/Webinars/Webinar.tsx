02daf8f477c59a7aa7268f578fff258fae9293a7
bradamorg007 <bradley.morgan190@gmail.com>
2020-04-13 20:35:25 +0100

    Made a start on routing

---
+++ src/pages/Webinars/Webinar.tsx
@@ -0,0 +1,15 @@
+import { IonPage } from '@ionic/react';
+import React from 'react';
+
+
+const Webinar: React.FC = () => {
+
+    return (
+        <IonPage>
+            <h2>Webinar Page</h2>
+        </IonPage>
+    )
+
+};
+
+export default Webinar;
