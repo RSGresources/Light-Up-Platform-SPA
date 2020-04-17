ada17cfaed581e43ddd12a8c5f7b6e1c58dff945
bradamorg007 <bradley.morgan190@gmail.com>
2020-04-17 20:04:26 +0100

    Page header to use for all

---
+++ src/components/PageHeader/PageHeader.jsx
@@ -0,0 +1,74 @@
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonSearchbar,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
+} from '@ionic/react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router';
+
+import './Webinar.scss'
+
+const PageHeader = () => {
+
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearchTextChange = (event) => {
+        setSearchText(event.target.value);
+    }
+
+    return (
+        <IonHeader className="header-page" >
+            <IonToolbar className="header-toolbar1" mode="md" >
+                <div className="container">
+                    <div className="container-toolbar-main">
+                        <div className="menu-title-contents">
+                            <IonButtons slot="start">
+                                <IonMenuButton></IonMenuButton>
+                            </IonButtons>
+                            <IonLabel className="menu-title">
+                                Specialist
+                            </IonLabel>
+                        </div>
+                        <IonSearchbar
+                            onChange={handleSearchTextChange}
+                            value={searchText}
+                            autocomplete="on"
+                            spellCheck={true}
+                            placeholder="Search..."
+                            inputMode="search"
+                            mode="ios"
+                            searchIcon={undefined}
+                            type="text"
+                        ></IonSearchbar>
+                    </div>
+                    <div className="container-toolbar-sub">
+                        <IonTitle className="page-title">Webinars</IonTitle>
+                        <IonLabel className="page-discription">Stay Connected: Find all the latest and upcoming live chats accross Light Up network</IonLabel>
+                        <IonSegment className="page-control-bar" value="Live" mode="md">
+                            <IonSegmentButton className="control-button1" value="Live">
+                                <IonLabel className="page-control-button-text1">
+                                    Live
+                                </IonLabel>
+                            </IonSegmentButton>
+                            <IonSegmentButton className="control-button3" value="scheduled">
+                                <IonLabel className="page-control-button-text2">
+                                    Scheduled
+                                </IonLabel>
+                            </IonSegmentButton>
+                        </IonSegment>
+                    </div>
+                </div>
+            </IonToolbar>
+        </IonHeader>
+    )
+
+};
+
+export default withRouter(PageHeader);
